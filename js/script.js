6739305ac84b282ef8485e055b765ed1e85b9873
tmpk009 <16647255@students.lincoln.ac.uk>
2019-01-09 12:26:28 +0000

    added basket

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ $(document).ready(function(){
     
     var headerHeight = $("header").height();
     console.log(headerHeight);
+    var count = 1;
     
-    var items = parseInt(localStorage.getItem('no_items'));
+    var no_basket = localStorage.getItem('no_items');
+    console.log(no_basket);
+    var items = parseInt(no_basket);
     
-    if (items!=0 || items<1){
+    if (no_basket == null){
         $('#basket').html('Basket(0)');
-        localStorage.setItem('no_items', 0);
+        items = 0;
     }
     
     $('#basket').html('Basket('+items+')');
@@ -31,21 +34,50 @@ $(document).ready(function(){
         });
     
         $('#basketadd').click(function(){
-            items+=1;
-            localStorage.setItem('no_items', items);
-            $('#basket').html('Basket('+items+')');
-            alert('item added to basket!');
+            if (items==1){
+                alert('Only per customer!');
+            }
+            else{
+                items+=1;
+                localStorage.setItem('no_items', items);
+                $('#basket').html('Basket('+items+')');
+                
+                alert('item added to basket!');
+                localStorage.setItem('is_there',true);
+                count++;
+            }
+            
+            
         });
     
     $('#basket').click(function(){
         $("#display_basket").css("visibility", "visible");
         $("#basket_back").css("visibility", "visible");
+        var present = localStorage.getItem('is_there');
+        if (present == 'true' && count==2){
+            $('#display_basket').append('<div id="selected"><p class="in_item">The Now Now - CD - £10.00 <img id="remove" src="assets/close_button.png"></p></div>');
+        }
+        if (present== 'true' && count==1){
+            $('#display_basket').append('<div id="selected"><p class="in_item">The Now Now - CD - £10.00 <img id="remove" src="assets/close_button.png"></p></div>');
+            count = count-1;
+        }
+        $('#close').click(function(){
+            $("#display_basket").css("visibility", "hidden");
+            $("#basket_back").css("visibility", "hidden");
+        });
+        
+        $('#remove').click(function(){
+            $('#selected').remove();
+        });
+        
     });
     
     $('#close').click(function(){
         $("#display_basket").css("visibility", "hidden");
         $("#basket_back").css("visibility", "hidden");
     });
+    
+    
     
     console.log('ready');
     
